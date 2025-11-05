0118b628a23090c9eec2a4f2355165bb36c5e68d
JSebas17 <61057636@continental.edu.pe>
2025-11-05 13:23:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def saludo(nombre: str) -> str:
-    return f"Hola, {nombre} !Biewnvenido a Git, guapo¡"
+    return f"Hola, {nombre} !Biewnvenido a Git, guapo y bello¡"
 
 if __name__ == "__main__":
     nombre =  input("¿Tu nombre?")
